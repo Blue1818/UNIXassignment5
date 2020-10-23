1116b76f3e9390d8034e31653b179cf00c2c57bd
Blue1818 <70177582+Blue1818@users.noreply.github.com>
2020-10-23 14:54:45 -0500

    Update main.cc

--- main.cc
+++ main.cc
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <sys/types.h>
-//#include <unistd.h>
-//#include <sys/wait.h>
+#include <unistd.h>
+#include <sys/wait.h>
 #include <stdio.h>
 #include <string.h>
 using namespace std;
