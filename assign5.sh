fbae33034beda4834b5d2e1d9a10fbc69ef9ca9d
Blue1818 <70177582+Blue1818@users.noreply.github.com>
2020-10-22 21:29:09 -0500

    Update assign5.sh

--- assign5.sh
+++ assign5.sh
@@ -4,10 +4,7 @@
 #    echo "hi"
 #}
 
-words=(word1 word2 word3)
 
-echo ${words[*]}
-echo ${words[1]}
 
-#read -p "command1? " line1[0]
-#parseCmd line1
+read -p "command1? " line1
+echo ${line1[1]}
