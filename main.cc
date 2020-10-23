6eb942c9e086142fcb9bfecc19b163bc935318ea
Blue1818 <70177582+Blue1818@users.noreply.github.com>
2020-10-23 15:06:24 -0500

    Update main.cc

--- main.cc
+++ main.cc
@@ -80,7 +80,7 @@ bool parseCmd(string* cmds, string bulk)
     //string temp = "";
     char* pch;
     int counter = 0;
-    char line[] = bulk.c_str();
+    char line[bulk.size()] = bulk.c_str();
 
     pch = strtok (line," ");
     while (pch != NULL)
